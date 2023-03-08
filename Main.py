b8f2a6da7552416e116947e6b40f0144be514c10
Samuel-de-Oliveira <samwolfg13@gmail.com>
2023-03-08 19:52:17 -0300

    Include the error treatment

--- Main.py
+++ Main.py
@@ -24,14 +24,22 @@ if len(sys.argv) <= 1:
 
 
 if __name__ == "__main__":
+    try:
+        # Arguments:
+        if sys.argv[1] in ("help", "-?"): helpMessage() # Help
+        elif sys.argv[1] in ("file", "-f"): createFile(sys.argv[2], sys.argv[3]) # Create file
+        elif sys.argv[1] in ("project", "-P"): createProject(sys.argv[2], sys.argv[3]) # Crate project
+        elif sys.argv[1] in ("version", "-v"): print(Version) # Show version Version
+        elif sys.argv[1] in ("append", "-a"): appendTemplate(sys.argv[2]) # Append a template
+        elif sys.argv[1] in ("remove", "-R"): removeTemplate(sys.argv[2]) # Remove a template
 
-    # Arguments:
-    if sys.argv[1] in ("help", "-?"): helpMessage() # Help
-    elif sys.argv[1] in ("file", "-f"): createFile(sys.argv[2], sys.argv[3]) # Create file
-    elif sys.argv[1] in ("project", "-P"): createProject(sys.argv[2], sys.argv[3]) # Crate project
-    elif sys.argv[1] in ("version", "-v"): print(Version) # Show version Version
-    elif sys.argv[1] in ("append", "-a"): appendTemplate(sys.argv[2]) # Append a template
-    elif sys.argv[1] in ("remove", "-R"): removeTemplate(sys.argv[2]) # Remove a template
+        # In case of user digit someting wrong
+        else: print("Sorry, this command doesn't exist. digit \'efc -?\' for commands") 
 
-    # In case of user digit someting wrong
-    else: print("Sorry, this command doesn't exist. digit \'efc -?\' for commands") 
+    except IndexError:
+        print('Missing arguments...\n'
+              'Type "efc -?" for details of the commands')
+
+    except Exception as Except:
+        print(f"An error has accured, the name of this error is {Except}"
+               "Please tell this to the software developer")
