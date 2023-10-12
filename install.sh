5bf838d40a08c19e3fc9fb1b63c16659cec999fc
Samuel-de-Oliveira <samwolfg13@gmail.com>
2023-10-12 10:37:49 +0000

    update installer

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ fi
 clear
 echo -e "\033[1mInstall program.\033[m"
 echo -e "\nVersion of Program: \033[33m1.0.1\033[m\n"
-echo -e "\nThe installing starts! This will not take long.\n"
 	
 sudo mkdir -p /usr/share/efc/
 sudo cp -r ./* /usr/share/efc/
@@ -18,6 +17,6 @@ cp -r langs/ ~/.efc/
 
 sudo cp efc /usr/bin/
 	
-echo -e "\nEverything is done!\n"
+echo -e "\nDone!\n"
 echo -e "\033[34;1mPress return to exit...\033[m"; read
 clear
