6978d678f82edc48d7e1c44e83444cdb8c3663b7
Samuel-de-Oliveira <samwolfg13@gmail.com>
2024-01-06 17:23:26 +0000

    First tests with Lua

--- 2.0-Lua/Main.lua
+++ 2.0-Lua/Main.lua
@@ -1,6 +1,27 @@
 -- Lua version of EFC Test
 
-local lib = require("library")
-local io  = require("io")
+-- Requires (To find the fuctions go to: 'library.lua')
+local lib  = require("library")
+local io   = require("io")
+local os   = require("os")
+local args = {...}
 
-io.write('Version: ' .. lib.version .. "\n")
+
+-- Program structure
+if args[1] then
+	for i,v in ipairs(args) do
+		-- Help message
+		if args[i] == '-h' then
+			lib.help()
+			os.exit()
+		end
+
+		-- Version
+		if args[i] == '-v' then
+			io.write('Version: ' .. lib.version .. '\n')
+		end
+	end
+else
+	-- In case of no arguments
+	lib.help()
+end
--- 2.0-Lua/library.lua
+++ 2.0-Lua/library.lua
@@ -1,6 +1,20 @@
+-- Requires
 local io = require("io")
+
+-- Constants and variables
 local library = {
 	version = "2.0"
 }
+
+
+-- Functions
+function library.help()
+	io.write(
+	'Usage: \"efc [command] [arguments]\"\n' ..
+	'-v: Show version.\n' ..
+	'-h: Show this message.\n'
+	)
+end
+
 
 return library
