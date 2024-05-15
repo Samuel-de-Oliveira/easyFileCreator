1c655c7f113e1db3fe65089e0b3746e60c6aef23
Samuel-de-Oliveira <samwolfg13@gmail.com>
2024-05-15 14:24:27 -0300

    Test firts json readings

--- 2.0-Lua/efc_library/init.lua
+++ 2.0-Lua/efc_library/init.lua
@@ -1,13 +1,14 @@
 -- Requires
 local io    = require("io")
 local os    = require("os")
+local json  = require("lunajson")
 local tools = require("efc_library/tools")
 
 -- Constants and variables
 local library = {
 	VERSION    = '2.0',
    CONFIGPATH = '~/.efc/',
-   EFCPATH  = '/usr/lib/efc/'
+   EFCPATH    = '/usr/lib/efc/'
 }
 
 ---- Functions ----
@@ -51,20 +52,28 @@ function library.createFile(fileName, Language)
 		os.exit()
 	end
 
+   -- Get config.json consts
+   local configFile = io.open(library.EFCPATH .. 'languages/' ..
+                              Language .. "/config.json", "r")
+   print(configFile:read('*all'))
+   local extension  = json.decode(configFile:read("*all")).extension
+   local mainFile   = json.decode(configFile:read("*all")).file
+   configFile:close()
+
    -- Verify if the file aready exists
-   if tools.fileExists(fileName .. '.lua') then
+   if tools.fileExists(fileName .. extension) then
       io.write("The file " .. fileName .. ".lua Alredy exists\n" ..
                'Please digit "efc -?" for help.\n')
       os.exit()
    end
-
-	-- Copy template files
+   
+   -- Copy template files
    tools.Copy(
       -- Select language
-      library.EFCPATH .. 'languages/lua/file.lua',
+      library.EFCPATH .. 'languages/' .. Language .. '/' .. mainFile,
 
       -- Write to filename
-      fileName .. '.lua'
+      fileName .. extension
    )
 end
 
