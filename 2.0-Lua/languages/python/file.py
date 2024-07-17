d94341f3eaa537f9d0a8b3d9aa9e2e642b5b3bdd
Samuel-de-Oliveira <samwolfg13@gmail.com>
2024-07-17 14:09:09 -0300

    Add Python template

---
+++ 2.0-Lua/languages/python/file.py
@@ -0,0 +1,5 @@
+def main() -> None:
+    print('Hello, world!')
+
+if __name__ == "__main__":
+    main()
---
+++ 2.0-Lua/languages/python/project/Main.py
@@ -0,0 +1,6 @@
+import library as lib
+
+hello = lib.Message('Hello, world!')
+
+if __name__ == '__main__':
+    hello.printMessage()
