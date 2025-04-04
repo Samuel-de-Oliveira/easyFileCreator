40fc2c7f6088054cf9dc071cd97be90daae31c65
Samuel-de-Oliveira <samwolfg13@gmail.com>
2025-04-04 08:51:43 -0300

    PEP 8

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-#--*--------- Easy File Creator (EFC) ---------*--#
+# --*--------- Easy File Creator (EFC) ---------*--#
 #                                                 #
 # This is a free software, thats mean you can     #
 # distibute, run and edit it freely, but you need #
@@ -9,7 +9,7 @@
 # License:       MIT                              #
 # Created by:    Samuel de Oliveira               #
 # Contribuitors: None                             #
-#--*-------------------------------------------*--#
+# --*-------------------------------------------*--#
 
 # Imports
 import sys
@@ -19,39 +19,52 @@ from lib import *
 from lib import __version__, __author__
 
 # If it hasn't no arguments
-if len(sys.argv) <= 1: 
+if len(sys.argv) <= 1:
     helpMessage()
     exit()
 
 
 ## Main code ##
-if __name__ == "__main__":
+if __name__ == '__main__':
     try:
         # Arguments:
-        if sys.argv[1] in ("help", "-?"): helpMessage() # Help
-        elif sys.argv[1] in ("file", "-f"): createFile(sys.argv[2], sys.argv[3]) # Create file
-        elif sys.argv[1] in ("project", "-P"): createProject(sys.argv[2], sys.argv[3]) # Crate project
-        elif sys.argv[1] in ("append", "-a"): appendTemplate(sys.argv[2]) # Append a template
-        elif sys.argv[1] in ("remove", "-R"): removeTemplate(sys.argv[2]) # Remove a template
-        elif sys.argv[1] in ("list", "-l"): showList() # Show list of templates
-
+        if sys.argv[1] in ('help', '-?'):
+            helpMessage()   # Help
+        elif sys.argv[1] in ('file', '-f'):
+            createFile(sys.argv[2], sys.argv[3])   # Create file
+        elif sys.argv[1] in ('project', '-P'):
+            createProject(sys.argv[2], sys.argv[3])   # Crate project
+        elif sys.argv[1] in ('append', '-a'):
+            appendTemplate(sys.argv[2])   # Append a template
+        elif sys.argv[1] in ('remove', '-R'):
+            removeTemplate(sys.argv[2])   # Remove a template
+        elif sys.argv[1] in ('list', '-l'):
+            showList()   # Show list of templates
 
         # Show version info
-        elif sys.argv[1] in ("version", "-v"):
-            print(f'Version: \033[1;32m{__version__}\033[m\n'
-                  f'This project is created by {__author__} (all right reserved)\n'
-                   'Visit the project\'s repository: \033[34mhttps://github.com/Samuel-de-Oliveira/easyFileCreator.\033[m')
+        elif sys.argv[1] in ('version', '-v'):
+            print(
+                f'Version: \033[1;32m{__version__}\033[m\n'
+                f'This project is created by {__author__} (all right reserved)\n'
+                "Visit the project's repository: \033[34mhttps://github.com/Samuel-de-Oliveira/easyFileCreator.\033[m"
+            )
 
         # In case of user digit someting wrong
-        else: print("Sorry, this command doesn't exist. digit \033[1;42mefc -?\033[m for commands") 
-   
+        else:
+            print(
+                "Sorry, this command doesn't exist. digit \033[1;42mefc -?\033[m for commands"
+            )
 
     # In case of dont have enough arguments
     except IndexError:
-        print('\033[1mMissing arguments...\033[m\n'
-              'Type \033[1;42mefc -?\033[m for details of the commands')
-    
+        print(
+            '\033[1mMissing arguments...\033[m\n'
+            'Type \033[1;42mefc -?\033[m for details of the commands'
+        )
+
     # In case of a unknown error happens
     except Exception as Except:
-        print(f"\033[1;31mAn error has accured, the name of this error is {Except}\033[m\n"
-               "Please tell this to the software developer")
+        print(
+            f'\033[1;31mAn error has accured, the name of this error is {Except}\033[m\n'
+            'Please tell this to the software developer'
+        )
--- lib/__init__.py
+++ lib/__init__.py
@@ -6,33 +6,43 @@ import json
 
 # Variable of version
 __version__: str = '1.0.2'
-__author__:  str = 'Samuel de Oliveira'
+__author__: str = 'Samuel de Oliveira'
+__all__: list = [
+    'Config',
+    'helpMessage',
+    'createFile',
+    'createProject',
+    'appendTemplate',
+    'showList',
+    'removeTemplate',
+]
 
 ## Define the paths of software in each OS.
 # Linux
-if os.name in ("linux", "posix"):
+if os.name in ('linux', 'posix'):
     programPath: str = '/usr/share/efc/'
-    configPath:  str = f'/home/{os.getlogin()}/.efc/'
+    configPath: str = f'/home/{os.getlogin()}/.efc/'
 # Windows
-elif os.name in ("nt", "dos"):
+elif os.name in ('nt', 'dos'):
     # TODO: rework this in the windows OS
     programPath: str = f'C:\\Users\\{os.getlogin()}\\AppData\\Roaming\\efc\\'
-    configPath:  str = programPath
+    configPath: str = programPath
     print(programPath)
 
     # For while it doesn't work for Windows yet :/
-    print('This software doesn\'t work for Windows yet')
+    print("This software doesn't work for Windows yet")
     exit()
 
 
-class Config():
+class Config:
     def __ini__(self) -> None:
         pass
 
 
 # Help
-def helpMessage():
-    print("""
+def helpMessage() -> None:
+    print(
+        """
 \tEasy File Creator (EFC) usage
 \tefc [Command...] (arguments...)
 
@@ -42,11 +52,12 @@ def helpMessage():
 
 \033[1mversion (-v):\033[m show version (efc version)
 \033[1mhelp (-?):\033[m show this help message (efc help)
-          """)
+          """
+    )
 
 
 # Create a file
-def createFile(fileName: str, Language: str):
+def createFile(fileName: str, Language: str) -> None:
     # Open config file of Template
     with open(f'{configPath}/langs/{Language}/config.json', 'r') as file:
         config: dict = json.loads(file.read())
@@ -54,22 +65,30 @@ def createFile(fileName: str, Language: str):
     # And then copy the template file and rename to name you've choosed
     try:
         # Verify if the file exists, to avoid replace a important file.
-        if os.path.exists(f'{fileName}{config["extension"]}'): raise FileExistsError
+        if os.path.exists(f'{fileName}{config["extension"]}'):
+            raise FileExistsError
 
-        shutil.copy(f"{configPath}/langs/{Language}/{config['file']}",
-                    f'{fileName}{config["extension"]}')
+        shutil.copy(
+            f"{configPath}/langs/{Language}/{config['file']}",
+            f'{fileName}{config["extension"]}',
+        )
 
-    except FileNotFoundError: print('\033[1;31mThe language doesn\'t exists!\033[m')
+    except FileNotFoundError:
+        print("\033[1;31mThe language doesn't exists!\033[m")
     except FileExistsError:
-        print(f'The file \033[1m{fileName}\033[m already exists, '
-               'then delete this file or choose a new name for it.')
+        print(
+            f'The file \033[1m{fileName}\033[m already exists, '
+            'then delete this file or choose a new name for it.'
+        )
     except Exception as Except:
-        print(f"\033[31mAn error occurred, the error name is {Except}.\033[m "
-               "Please tell this with the developer!")
+        print(
+            f'\033[31mAn error occurred, the error name is {Except}.\033[m '
+            'Please tell this with the developer!'
+        )
 
 
 # Create a project
-def createProject(projectName: str, Language: str):
+def createProject(projectName: str, Language: str) -> None:
     # Open config file of Template
     with open(f'{configPath}/langs/{Language}/config.json', 'r') as file:
         config: dict = json.loads(file.read())
@@ -78,51 +97,67 @@ def createProject(projectName: str, Language: str):
     if config['project']:
         # And then copy the template project and rename to name you've choosed
         try:
-            shutil.copytree(f"{configPath}langs/{Language}/project/",
-                            f'{projectName}')
+            shutil.copytree(
+                f'{configPath}langs/{Language}/project/', f'{projectName}'
+            )
 
-        except FileNotFoundError: print('The language doesn\'t exists')
+        except FileNotFoundError:
+            print("The language doesn't exists")
         except FileExistsError:
-            print(f'The directory \033[1m{projectName}\033[m already exists, '
-                   'then delete this directory or choose a new name for it.')
+            print(
+                f'The directory \033[1m{projectName}\033[m already exists, '
+                'then delete this directory or choose a new name for it.'
+            )
         except Exception as Except:
-            print(f"\033[1;31mAn error occurred, the error name is {Except}. \033[m"
-                   "Please tell this with the developer!")
-    else: print('there\'s no projects for this language...')
+            print(
+                f'\033[1;31mAn error occurred, the error name is {Except}. \033[m'
+                'Please tell this with the developer!'
+            )
+    else:
+        print("there's no projects for this language...")
+
 
 # Append template
-def appendTemplate(Dir: str):
+def appendTemplate(Dir: str) -> None:
     # FIXME: The software cant append any template for any reason, need fix this urgent.
     try:
         if os.path.exists(f'{Dir}/config.json'):
-            shutil.copy(f"./{Dir}",
-                        f"{configPath}/langs/")
-        else: raise FileNotFoundError
+            shutil.copy(f'./{Dir}', f'{configPath}/langs/')
+        else:
+            raise FileNotFoundError
 
-    except FileNotFoundError: print("Directory not found!")
+    except FileNotFoundError:
+        print('Directory not found!')
     except Exception as Except:
-        print(f"\033[1;31mAn error occurred, the error name is {Except}. \033[m"
-               "Please tell this with the developer!")
+        print(
+            f'\033[1;31mAn error occurred, the error name is {Except}. \033[m'
+            'Please tell this with the developer!'
+        )
 
 
-def showList():
+def showList() -> None:
     # Print all templates
-    langsList: list = os.listdir(f"{configPath}/langs/")
-    print("Your language templates:")
+    langsList: list = os.listdir(f'{configPath}/langs/')
+    print('Your language templates:')
     for lang in langsList:
         print(f'\033[1m{lang}\033[m', end='  ')
-    
+
     # New line
     print()
 
 
 # Remove template
-def removeTemplate(Language: str):
-    try: shutil.rmtree(f"{configPath}/langs/{Language}")
-    except FileNotFoundError: print('This template doesn\'t exist!')
+def removeTemplate(Language: str) -> None:
+    try:
+        shutil.rmtree(f'{configPath}/langs/{Language}')
+    except FileNotFoundError:
+        print("This template doesn't exist!")
     except Exception as Except:
-        print(f"\033[1;31mAn error occurred, the error name is {Except}. \033[m"
-               "Please tell this with the developer!")
+        print(
+            f'\033[1;31mAn error occurred, the error name is {Except}. \033[m'
+            'Please tell this with the developer!'
+        )
 
 
-if __name__ == "__main__": print("Please, open the \'Main.py\' file.")
+if __name__ == '__main__':
+    print("Please, open the 'Main.py' file.")
